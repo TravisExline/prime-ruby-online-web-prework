a37bdda866b5f5455a9045030fd93c642296b37c
Travis Exline <t.exline22@gmail.com>
2019-04-30 17:39:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,18 @@
-
-def prime?(new_array)
-  if new_array < 0 or new_array == 0 or 1
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
     return false
   else
-    (2..num-1).to_a.all? do |numbers|
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
+end
+
+def prime?(number)
+  if number < 0 or number == 0 or number = 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_number|
       new_array % numbers != 0
     end
   end
