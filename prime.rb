e4078d0d9ef9925c4bd59f60adf210671cda719f
Travis Exline <t.exline22@gmail.com>
2019-04-30 17:38:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(new_array)
   if new_array < 0 or new_array == 0 or 1
     return false
   else
-    (2...num-1).to_a.all? do |numbers|
+    (2..num-1).to_a.all? do |numbers|
       new_array % numbers != 0
     end
   end
